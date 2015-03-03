5e547ce357746eda4b550b2673102c7022d1cf6e
niinyarko <niinyarko@yahoo.com>
2015-03-03 19:31:06 +0000

    added aldeed:collection2 package to project

---
+++ mlog/lib/collections.js
@@ -0,0 +1,30 @@
+Videos = new Mongo.Collection("videos");
+
+Videos.attachSchema(new SimpleSchema({
+  caption: {
+    type: String,
+    label: "Add a caption"
+  },
+  owner: {
+    type: String,
+    autoform: {
+      omit: true
+    },
+    autoValue: function() {
+      if(this.isInsert) {
+        return this.userId;
+      }
+    }
+  },
+  createdAt: {
+    type: Date,
+    autoform: {
+      omit: true
+    },
+    autoValue: function() {
+      if(this.isInsert) {
+        return new Date();
+      }
+    }
+  }
+}))
